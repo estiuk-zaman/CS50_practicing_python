8adc8ec17411c6c5ff1d3ffa1cb7bcb0fe205673
Estiuk uz zaman <mdestiukuzzaman@gmail.com>
2026-04-08 04:06:05 +0600

    case x|y

--- test_python.py
+++ test_python.py
@@ -1,10 +1,10 @@
 name = input("Whats your name? ")
 
 match name:
-    case "Estiuk":
-        print("Hlw v2")
-    case "istii":
-        print("Hlw v1")
+    case "Estiuk" | "istii":
+        print("Hlw v2 or v1")
+    # case "istii":
+    #     print("Hlw v1")
     case "Oishik":
         print("Hlw old")
     case _:
