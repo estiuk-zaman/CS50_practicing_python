a01957bc03d4f2e8218c6cd41db8487764071050
Estiuk uz zaman <mdestiukuzzaman@gmail.com>
2026-04-08 04:03:16 +0600

    Match

--- test_python.py
+++ test_python.py
@@ -1,7 +1,9 @@
 name = input("Whats your name? ")
-if name == "istii":
-    print("Hlw new one")
-elif name == "Estiuk":
-    print("Hlw new one v2")
-elif name =="Oishik":
-    print("Hlw old one")
+
+match name:
+    case "Estiuk":
+        print("Hlw v2")
+    case "istii":
+        print("Hlw v1")
+    case "Oishik":
+        print("Hlw old")
